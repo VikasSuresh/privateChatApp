3e3a30455b82fe62be41b6f2816c6d502ec91166
Vikas <vsvikassvs@gmail.com>
2020-03-19 15:45:26 +0530

    .

--- server/routes/user.js
+++ server/routes/user.js
@@ -65,7 +65,7 @@ router.post('/login',(req,res)=>{
                                 result.save()
                             }
                         })
-                        res.cookie('token',token,{domain:process.env.client,sameSite:"none",secure}).send({
+                        res.cookie('token',token,{sameSite:"none",secure}).send({
                             success:true
                         })
                     })
